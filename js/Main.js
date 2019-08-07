bb68f37b5764dbd72fe7e5355873c80958a52f0c
davidlechuga <david.lechuga@lakmisystems.com.mx>
2019-08-07 13:30:40 -0500

    firt-files

---
+++ js/Main.js
@@ -0,0 +1,25 @@
+console.log("Hola mundo from main.js")
+console.log(24*365)
+console.log(3==3)
+console.log(3==='3')
+console.log(false || false)
+console.log ((age+10) ==29) && true
+
+//console es un obejeto (console)
+// metodo es una funcion dentro de un objeto  (.log)
+// metodos  tienen propositos: mostrar en pantallla un argumento.
+// poner el OBJETO "CONSOLE" en la consola, para encontrar métodos de "CONSOLE".
+
+var age = 22  //  a la variable edad , le asignamos el número 22.
+
+var livingDays = 365 * age  // 
+
+// palabras de asignacion
+// var =  un espacio en memoria para almacenar un tipo de dato 
+
+
+var division = 3 / 4000
+
+//var multiplicacion = 
+
+
