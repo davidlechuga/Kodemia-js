70befacf1838404d5a01400ce9a67c9cce179f60
davidlechuga <david.lechuga@lakmisystems.com.mx>
2019-08-12 10:56:03 -0500

    complete

--- Main.js
+++ Main.js
@@ -13,8 +13,6 @@ while(number < 10) {
 }
 
 
-
-
 //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::://
 //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::://
 //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::://
@@ -91,7 +89,7 @@ var dogObj = {
 //  dogObj.name  ** es la propiedad del objeto**
 //  metodos son funciones dentro de objetos.
 
-dodObj ['favoriteFood'] = Pizza
+dodObj ['favoriteFood'] = 'Pizza'
 //se inserta un nuevo key value al objeto
 
 
@@ -138,189 +136,5 @@ Object.getOwnPropertyNames(carro)
 ///   descripscion de la key de un objeto
 
 Object.getOwnPropertyDescriptor(carro , 'color')
-
-
-
-
-//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::://
-//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::://
-//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::://
-        // ------------- Exercise 11 --------------- //
-        
-      //metdos son funciones como propiedades de los objetos
-      //  las acciones son metodos
-
-
-var dogObj = {
-    'name': 'DEWEY',
-    'breed': 'Dash Hound',
-    'bark': () => 'Guaaau'
-}
-
-//  acceder a la funcion del objeto
-dogObj.bark
-//  acceder a la accion de la funcion
-dogObj.bark()
-
-
-
-var person  = {
-    firtName: 'Luis',
-    lastName: 'Ortiz',
-    age: 30,
-    greeting: function (){
-        return `Hola soy ${this.firtName}` 
-    }
-}
-
-//metodos pordemos ocupar las propiedades del objetos
-//.this es el scope de nuestros objetos
-//arrow function no tienen scope  y es global
-
-var DLH =
-[
-
-    {
-    'name' : 'History',
-    'score' : 9
-    },
-
-    {
-    'name' : 'Politica',
-    'score' : 7
-    },
-
-    {
-    'name' : 'Filosofía',
-    'score' : 5
-    },
-
-   { 
-    'name' : 'Estadística',
-    'score' : 10 
-   }
-
-]
-
-
-
-var arrays = DLH.map (function (DLH) {
-    return DLH.score
-} )
-
-
-
-// desestructuracion , sacar sus propiedades y alamacenarla
-
-
-//  CESAR INCRIPTING
-
-// DADO UN NUMERO VAMOS A AUMENTAR 
-
-
-//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::://
-//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::://
-//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::://
-        // ------------- Exercise 12 --------------- //
-
-//  propiedades de los objetos en camel case
-//  accesors de js
-//  template literal  `${this.height.value} $ {this.geiht.unit}`
-// accesors
-// con el setter le damos formato a las propiedades.
-// GET   <----->  SETTERS
-
-var persona = {
-
-name: 'themlma',
-heightValue: '165',
-birthDate: '1995-14-11',
-
-weight: {
-    unit: 'Kg',
-    value: '70'
-        },
-
-height: {
-    unit: 'cm',
-    value: '170'
-        },
-
-
-get getHeight() {
-    retun `${this.height.value} $ {this.geiht.unit}`
-}
-
-              }
-
-
-
-var street = {
-
-pavementMaterial: {
-        pavementConcrete: 'concrete',
-        pavementAsphalt: 'asphalt',
-        pavementCobble: 'cobble'
-                   },
-
-luminaires: {
-
-        lumenesValue: '600',
-        watts: '12'
-
-            },
-
-garbageDumps: {
-
-    sizeGarbageDumps: 
-    [ 'lg','md','lg' ],
-
-    materialGarbageDumps: 
-    [ 'plastic', 'metal', 'organic material'],
-
-    typesOfGarbage: 
-
-    [ 'Metal', 'E-waste', 'Paper', 'Glass',  ]
-                                                  
-            }
-
-              }
-
-
-
-
-
-//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::://
-//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::://
-//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::://
-        // ------------- Exercise 13 --------------- //
-
-// paso un constructor que paso por el objeto console /window
-// pasaron por una funcion contructura
-//  constructor = define el molde de un objeto en general
-//  funciones constructoras CAPITALIZE no CAMEL CASE   DogObjet    //  (no)  dogObj
-// no puedes ser anonimas , ni arrow porque no tienen scope
-//  dado palabra function reservada tenemos acceso a scope 
-
-
-//  almacenando en memoria una nueva instaciacion del objeto DOG
-
-
-// Constructor function
-
-function Dog(breed, name, size) {
-    this.breed = breed
-    this.name = name
-    this.size = size
-  }
-  var firstDog = new Dog('Dash hound', 'Sr. Salchicha', 'S')
-  
-  
-  
-  
-
-
-
-
 
 
