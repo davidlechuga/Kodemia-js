5b94a4e95980968895d19247388799bc21f53858
davidlechuga <david.lechuga@lakmisystems.com.mx>
2019-08-19 13:21:47 -0500

    post

--- Main.js
+++ Main.js
@@ -1,4 +1,8 @@
-const URL_demo = 'https://ajaxkode.farebaseio.com/'
+
+
+const URL_demo = 'https://ajaxkode.firebaseio.com/'
+
+
 
 var xhttp = new XMLHttpRequest()
 xhttp.onreadystatechange = function () {
@@ -10,5 +14,41 @@ xhttp.onreadystatechange = function () {
         console.log('No encontre nada perro');
     }
 }
-xhttp.open("GET",  '${URL_demo}')
+xhttp.open("GET", `${URL_demo}.json`)
 xhttp.send()
+345
+
+
+
+
+/*
+
+///////POST  ////////
+
+const URL_demo = 'https://ajaxkode.firebaseio.com/'
+
+const names = {
+        'firstName': 'Daniel',
+        'lastName': 'Lechuga',
+        'age' : 24
+              }
+
+        
+ const name = JSON.stringify(names)
+
+
+
+var xhttp = new XMLHttpRequest()
+xhttp.onreadystatechange = function () {
+    // console.log(this.readyState, this.status)
+    if (this.readyState === 4 && this.status === 200) {
+        console.log(this.response);
+        document.write(this.response)
+    } else if (this.readyState === 4 && this.status === 404) {
+        console.log('No encontre nada perro');
+    }
+}
+xhttp.open("POST", `${URL_demo}.json`)
+xhttp.send(`${name}`)
+
+*/
